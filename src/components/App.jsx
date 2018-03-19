1e495dd5cda82bca82e18012f6ddbae17750d3eb
Jan Calanog <jan.calanog@iteratec.at>
2018-03-19 14:44:09 +0100

    Added github link

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,14 +6,28 @@ import Preview from '../containers/Preview.jsx';
 
 
 const App = () => (
-    <div className="section">
-        <div className="container is-fluild">
-            <div className='columns'>
-                <div className='column is-4' style={{ backgroundColor: 'whitesmoke' }}>
-                        <DishesEditor />
-                </div>
-                <div className='column is-8' style={{ backgroundColor: 'lightslategrey' }}>
-                        <Preview />
+    <div>
+        <div style={{padding: '1rem 0 0 0'}}>
+            <nav className="level">
+                <p className="level-item has-text-centered">
+                    <a className="button is-small" target="_blank" rel="noopener noreferrer" href="https://github.com/reakAleek/react-redux-example-application">
+                        <span className="icon">
+                        <i className="fa fa-github"></i>
+                        </span>
+                        <span>GitHub</span>
+                    </a>
+                </p> 
+            </nav>
+        </div>
+        <div className="section">
+            <div className="container is-fluild">
+                <div className='columns'>
+                    <div className='column is-4' style={{ backgroundColor: 'whitesmoke' }}>
+                            <DishesEditor />
+                    </div>
+                    <div className='column is-8' style={{ backgroundColor: 'lightslategrey' }}>
+                            <Preview />
+                    </div>
                 </div>
             </div>
         </div>
